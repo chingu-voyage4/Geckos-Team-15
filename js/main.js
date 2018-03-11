0d78e17d2fea1780b7af3fc5e6c30acc6d5a83c7
Seth Christmus <beboptank12@gmail.com>
2018-03-11 15:01:10 +0900

    Changes to js, images, and css

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ copy.textContent += year;
 //start of slideshow banner js
 var i = 0;
 var images = [];
-var time = 10000;
+var time = 5000;
+var counter = 0;
 
 // image list
 images[0] = "images/thomashafeneth-holding-flowers-unsplash.jpg";
@@ -16,34 +17,33 @@ images[3] = "images/jellekevanooteghem-child-unsplash.jpg";
 
 // change image
 function changeImage(){
-	document.slide.src = images[i];
+	document.getElementById("hero").style.backgroundImage = "url("+images[i]+")";
 
 	if (i < images.length - 1){
 		i++;
 	} else {
 		i = 0;
 	}
-
 	setTimeout("changeImage()", time);
 }
 // change image by clicking arrows
 document.getElementById("nextBtn").addEventListener("click", function() {
 	if (i < images.length - 1) {
 		i++;
-		document.slide.src = images[i];
+		document.getElementById("hero").style.backgroundImage = "url("+images[i]+")";
 	} else {
 		i = 0;
-		document.slide.src = images[i];
+		document.getElementById("hero").style.backgroundImage = "url("+images[i]+")";
 	}
 });
 
 document.getElementById("prevBtn").addEventListener("click", function() {
 	if (i > 0) {
 		i--;
-		document.slide.src = images[i];
+		document.getElementById("hero").style.backgroundImage = "url("+images[i]+")";
 	} else {
 		i = images.length - 1;
-		document.slide.src = images[i];
+		document.getElementById("hero").style.backgroundImage = "url("+images[i]+")";
 	}
 });
 
